acb5f232b58f86ec0ce6b44c9f5476d74d5a7927
Minh Au <auduongminh2004@gmail.com>
2022-03-30 22:47:26 -0600

    initial commit

---
+++ out/source/The_Maze_Runner.java
@@ -0,0 +1,33 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class The_Maze_Runner extends PApplet {
+
+public void setup() {
+    
+}
+
+public void draw() {
+    background(100);    
+}
+  public void settings() {  size(1080, 720); }
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "The_Maze_Runner" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
